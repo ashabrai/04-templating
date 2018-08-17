7150eb60d8d695bf3379e835edefaac2ae0de1cb
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-08-17 11:58:13 -0700

    added things to index and js

--- heather-brai/scripts/article.js
+++ heather-brai/scripts/article.js
@@ -12,27 +12,31 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  let compiledTemplate = Handlebars.compile( $('#article-template').html() );
+
+  // TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars. completed!
+
+
 
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-
-
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+  return compiledTemplate(this);
+}
 
-  // REVIEW: The ternary operator above accomplishes this same logic.
-  // if(this.publishedOn) {
-  //   this.publishStatus = `published ${this.daysAgo} days ago`;
-  // } else {
-  //   this.publishStatus = '(draft)';
-  // }
+// REVIEW: The ternary operator above accomplishes this same logic.
+// if(this.publishedOn) {
+//   this.publishStatus = `published ${this.daysAgo} days ago`;
+// } else {
+//   this.publishStatus = '(draft)';
+// }
 
-  // TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+// TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
 
-};
+
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
 // PUT YOUR RESPONSE HERE
